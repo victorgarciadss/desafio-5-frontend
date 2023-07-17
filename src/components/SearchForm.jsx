61bc0d20b9a59dec395e244d9fae5dc8e8638201
victorgarciadss <garciavictor.1308@gmail.com>
2023-07-16 23:56:54 -0300

    Feat: requisição de pesquisa com filtros funcionando

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,27 +1,82 @@
+import { useState } from "react"
 import "../css/SearchForm.css"
 
 const SearchForm = () => {
 
-    return(
-        <form className='form-container' action="">
-          <div className="labels">
-            <label htmlFor="">
-              Data de Início <br />
-              <input type="date" className='input' id="" />
-            </label>
-            <label htmlFor="">
-              Data de Fim <br />
-              <input type="date" className='input' id="" />
-            </label>
-            <label htmlFor="">
-              Nome do operador transacionado <br />
-              <input type="text" className='input' />
-            </label>
-          </div>
+  const [dataInicio, setDataInicio] = useState('');
+  const [dataFim, setDataFim] = useState('');
+  const [nomeOperadorTransacao, setNomeOperadorTransacao] = useState('');
 
-          <button type="submit">Pesquisar</button>
-        </form>
-    )
+  async function submitDataWithAllFilters(e) {
+    e.preventDefault();
+
+    if(dataInicio == "" && dataFim == "" && nomeOperadorTransacao == ""){
+      alert("Insira dados se quiser um filtro!");
+      return;
+    }
+    else if(dataInicio == "" && dataFim == ""){
+      try {
+        const response = await fetch("http://localhost:8080/transferencias/nome", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify({ dataInicio, dataFim, nomeOperadorTransacao })
+        });
+  
+        const responseData = await response.json();
+        console.log(responseData);
+  
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    else{
+      try {
+        const response = await fetch("http://localhost:8080/transferencias/periodo", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify({ dataInicio, dataFim, nomeOperadorTransacao })
+        });
+  
+        const responseData = await response.json();
+        console.log(responseData);
+  
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    
+
+    
+
+  }
+
+  return (
+    <form 
+      className='form-container'
+      onSubmit={submitDataWithAllFilters}
+      action="">
+      <div className="labels">
+        <label htmlFor="">
+          Data de Início <br />
+          <input type="date" onChange={(e) => setDataInicio(e.target.value)} className='input' id="" />
+        </label>
+        <label htmlFor="">
+          Data de Fim <br />
+          <input type="date" onChange={(e) => setDataFim(e.target.value)} className='input' id="" />
+        </label>
+        <label htmlFor="">
+          Nome do operador transacionado <br />
+          <input type="text" onChange={(e) => setNomeOperadorTransacao(e.target.value)} className='input' />
+        </label>
+      </div>
+
+      <button type="submit">Pesquisar</button>
+    </form>
+  )
 }
 
 export default SearchForm;
